87bbe914666f4f2e77cbd49e4a607f5e20c1bd6b
Harsh Deep <harshdeep7887@gmail.com>
2023-09-02 16:59:17 +0530

    package.json edit 7

--- src/conn.js
+++ src/conn.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 // const databaseUrl = process.env.PORT || "mongodb://localhost:27017/SchoolStudentData"
 
-const databaseUrl = process.env.PORT || 'mongodb+srv://harshdeep7887:harsh@123@cluster0.z0ob0jf.mongodb.net/?retryWrites=true&w=majority/SchoolStudentData' || 'mongodb://localhost:27017/SchoolStudentData'
+const databaseUrl = process.env.PORT || process.env.MONGO_URI || 'mongodb://localhost:27017/SchoolStudentData'
+
+// const databaseUrl = process.env.PORT || 'mongodb+srv://harshdeep7887:harsh@123@cluster0.z0ob0jf.mongodb.net/?retryWrites=true&w=majority/SchoolStudentData' || 'mongodb://localhost:27017/SchoolStudentData'
 
 
 mongoose.connect(databaseUrl,{
